1ec1efa0e1e23a316a638ab9b989fd3c7b7571b2
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-06-05 14:21:50 +0900

    refactor: use powershell instead of ts

---
+++ src/configs/.dynamic/alias.ps1
@@ -0,0 +1,16 @@
+$aliasDir = ".\dist\dotfiles\exact_alias"
+
+$aliases = @(
+    @{ aliasName = "npm"; commandName = "pnpm %*" },
+    @{ aliasName = "rnpm"; commandName = "C:\Program Files\nodejs\npm %*" }
+)
+
+New-Item -ItemType "Directory" -Path $aliasDir -Force | Out-Null
+Get-ChildItem -Path $aliasDir -File | ForEach-Object {
+    Remove-Item -Path $_.FullName -Force
+}
+
+foreach ($alias in $aliases) {
+    $filePath = Join-Path $aliasDir ("$($alias.aliasName).cmd")
+    New-Item -Path $filePath -Value $alias.commandName -Force | Out-Null
+}
