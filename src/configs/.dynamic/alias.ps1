fd18f2c1c8bb3d7b5c8fde9432d3547ac67bd2a4
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-06-14 18:19:06 +0900

    feat: add uv-python-update command

--- src/configs/.dynamic/alias.ps1
+++ src/configs/.dynamic/alias.ps1
@@ -2,6 +2,7 @@ $aliasDir = ".\dist\dotfiles\exact_alias"
 
 $aliases = @(
     @{ aliasName = "mr"; commandName = "mise run %*" }
+    @{ aliasName = "uv-python-update"; commandName = "uv python install --reinstall 3.11 3.12 3.13" }
 )
 
 New-Item -ItemType "Directory" -Path $aliasDir -Force | Out-Null
