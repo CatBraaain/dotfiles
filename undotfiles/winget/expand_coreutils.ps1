aae8f64b097c79c651601e62025a76831d79d769
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-01 04:30:46 +0900

    fix: error when dir not exits

--- undotfiles/winget/expand_coreutils.ps1
+++ undotfiles/winget/expand_coreutils.ps1
@@ -1,9 +1,11 @@
 $coreutils_path = "${env:LOCALAPPDATA}\Microsoft\WinGet\Links\coreutils.exe"
+$expand_path = "${env:USERPROFILE}\.local\coreutils"
+New-Item -Path $expand_path -ItemType Directory -Force
 $utilsList = @(coreutils --list)
 $utilsList | ? {
     $_ -notin @("[")
 } | % {
-    "${env:USERPROFILE}\.local\coreutils\${_}.exe"
+    "${expand_path}\${_}.exe"
 } | ? {
     -not (Test-Path $_)
 } | % {
