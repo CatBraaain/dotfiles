7f0f3fbf9b1d79859ebebb41eb791e177e65a383
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-01 19:44:30 +0900

    chore: add memo comment

--- undotfiles/winconfig/my-custom.ps1
+++ undotfiles/winconfig/my-custom.ps1
@@ -3,6 +3,8 @@ reg add "HKEY_CLASSES_ROOT\AllFilesystemObjects\shell\pintohome" /v LegacyDisabl
 reg add "HKEY_CLASSES_ROOT\Drive\shell\pintohome" /v LegacyDisable /f
 reg add "HKEY_CLASSES_ROOT\Folder\shell\pintohome" /v LegacyDisable /f
 reg add "HKEY_CLASSES_ROOT\Network\shell\pintohome" /v LegacyDisable /f
+# reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\CLSID\{679f85cb-0220-4080-b29b-5540cc05aab6}\shell\pintohome" /v LegacyDisable /f
+# reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\CLSID\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}\shell\pintohome" /v LegacyDisable /f
 
 # set high performance power plan
 $highPerfPlan = "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
