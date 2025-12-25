a919b9b24c3b07f08d8d2f4dbcc19cab14dd52a5
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-12-25 10:52:27 +0900

    feat: remove default alias from powershell

--- dotfiles/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::InputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
+
+Import-Module Microsoft.PowerShell.Management
+Get-Alias | % { if ($_.Name -notin @("%", "?")) { Remove-Item -Force "Alias:$($_.Name)" } }
