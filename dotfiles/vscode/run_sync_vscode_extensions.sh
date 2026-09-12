b7db2ca1b316f37f7fbb653b41ebf44ee6ed8501
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-09-12 11:17:24 +0900

    fix(vscode): install todo-lsp deps and point VSIX path at dist

--- dotfiles/vscode/run_sync_vscode_extensions.sh
+++ dotfiles/vscode/run_sync_vscode_extensions.sh
@@ -123,12 +123,10 @@ build_local_extension_vsix() {
       ;;
     todo-lsp.todo)
       version="$(cd "$todo_lsp_repo/vscode-todo" && node -p "require('./package.json').version")"
-      vsix_path="$todo_lsp_repo/vscode-todo/todo-$version.vsix"
+      vsix_path="$todo_lsp_repo/vscode-todo/dist/todo-$version.vsix"
 
       rm -f "$vsix_path"
-      (cd "$todo_lsp_repo" && just package) >&2
-      (cd "$todo_lsp_repo/vscode-todo" && npm run package -- \
-        --allow-missing-repository --no-rewrite-relative-links) >&2
+      (cd "$todo_lsp_repo" && npm ci && just package) >&2
       ;;
     *)
       echo "error: unknown local extension '$ext_id'" >&2
